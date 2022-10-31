e356f0c63b7f92a4fc413dd483d200ade3bca1ba
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-10-31 22:38:05 +0500

    video 56
    5-seprate statefull wiget in different page(input_page.dart)

---
+++ bmicalc/lib/input_page.dart
@@ -0,0 +1,22 @@
+class inputpage extends StatefulWidget {
+  const inputpage({Key? key}) : super(key: key);
+
+  @override
+  State<inputpage> createState() => _inputpageState();
+}
+
+class _inputpageState extends State<inputpage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+          body:  Center(
+            child: Text("test"),
+          )
+      ),
+
+    );
+
+  }
+}
--- bmicalc/lib/main.dart
+++ bmicalc/lib/main.dart
@@ -20,26 +20,5 @@ class bmicalc extends StatelessWidget {
     );
   }
 }
-class inputpage extends StatefulWidget {
-  const inputpage({Key? key}) : super(key: key);
 
-  @override
-  State<inputpage> createState() => _inputpageState();
-}
-
-class _inputpageState extends State<inputpage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-
-        body:  Center(
-            child: Text("test"),
-          )
-      ),
-
-    );
-
-  }
-}
 
