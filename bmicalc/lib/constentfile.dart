33ebba9c8786da86d0977c73e769f1905914b43f
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-11-05 13:04:18 +0500

    video 69
    1- in this video we design result page

--- bmicalc/lib/constentfile.dart
+++ bmicalc/lib/constentfile.dart
@@ -13,3 +13,21 @@ const Klargebuttonstyle=TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+const Ktittlestyle2=TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
--- bmicalc/lib/resultfile.dart
+++ bmicalc/lib/resultfile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'constentfile.dart';
+import 'container.dart';
+import 'input_page.dart';
 class ResultScreen extends StatelessWidget {
 
 
@@ -10,7 +13,51 @@ class ResultScreen extends StatelessWidget {
 
       ),
       body:
-      Text("body data"),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch ,
+        children: [
+          Expanded(child: Container(
+            child: Center(
+              child: Text('Your Result',style: Ktittlestyle2,),
+            ),
+          )),
+          Expanded(
+              flex: 5,
+              child: RepeatContainerCode(
+                onpressed: (){},
+                colors: activeColor,
+                cardWidgets: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center ,
+                  children: [
+                    Text('Normal',style: kResultTextStyle,),
+                    Text('18.3',style: kBMITextStyle,),
+                    Text('BMI is Low You should have to work More',textAlign: TextAlign.center, style: kBodyTextStyle,
+                    ),
+
+                  ],
+                ),
+              )
+          ),
+          Expanded(child: GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>InputPage()));
+
+            },
+            child: Container(
+              child: Center(child: Text('ReCalculate',style: Klargebuttonstyle,)),
+              color:Color(0xFFEB1555),
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 70.0,
+
+            ),
+          )),
+
+
+        ],
+      )
     );
   }
 }
