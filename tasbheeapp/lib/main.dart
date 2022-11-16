eb7b5e39cf50945579343b899c1243e2a76995dd
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-11-16 22:23:19 +0500

    setup of icon appbar design

--- tasbheeapp/lib/main.dart
+++ tasbheeapp/lib/main.dart
@@ -12,7 +12,40 @@ class tasbhee extends StatefulWidget {
 class _tasbheeState extends State<tasbhee> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+    appBar: AppBar(
+    centerTitle: true,
+    title: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Container(
+    height: 30,
+    width: 30,
+    child:Image(
+    image: AssetImage("assets/tasbhee-removebg-preview.png"),
+    fit: BoxFit.cover),
+    ),
+
+
+    const SizedBox(
+    width: 10,
+    ),
+    const Text(
+    'Tasbhee App',
+    style: TextStyle(color: Colors.amber),
+    ),
+    ],
+    ),
+    backgroundColor: Colors.black87, //<-- SEE HERE
+
+    ),
+    body:
+    Container(
+    )
+    )
+    );
   }
 }
 
