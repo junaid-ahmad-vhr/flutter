3c8acc918365bb48370bfc618f82dca2150721d0
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-11-17 20:22:00 +0500

    Screen1 and dialogbox complete

--- tasbheeapp/lib/main.dart
+++ tasbheeapp/lib/main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MyApp());
 }
-
+int Counter=0;
+String tnmae="Astagfirullah";
+final Name = TextEditingController();
+final number = TextEditingController();
+String varname=Name.text;
+int  varnumber=int.parse(number.text);
+String ? v;
+List<ElevatedButton> mylist = [];
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: tasbhee() //create new class for 'home' property of MaterialApp()
-      
+
     );
   }
 }
@@ -73,18 +81,84 @@ class _tasbheeState extends State<tasbhee> {
                         builder: (BuildContext context) {
                           return Expanded(
                             child: AlertDialog(
-                              title: Text('Welcome'),
-                              content: Text('GeeksforGeeks'),
+
+                              title: Text('Custom Tesbhee '),
+                              content: Container(
+                                height: 200,
+                                child: Column(
+                                  children: [
+                                    Text('Tesbhee Name'),
+
+                                    TextField(
+
+                                      controller:  Name,
+                                      decoration: InputDecoration(
+                                        hintText: "Allah",
+                                        border: OutlineInputBorder(),
+                                        suffixIcon: IconButton(
+                                            onPressed:(){
+                                              Name.clear();
+                                            },
+                                            icon: const Icon(Icons.clear_rounded)
+                                        ),
+
+
+                                      ),
+
+
+                                    ),
+                                    Text('Tasbhee Count'),
+                                    TextField(
+
+                                      controller:  number,
+                                      decoration: InputDecoration(
+                                        hintText: "plese enter less then 10000",
+                                        border: OutlineInputBorder(),
+                                        suffixIcon: IconButton(
+                                            onPressed:(){
+                                              number.clear();
+                                            },
+                                            icon: const Icon(Icons.clear_rounded)
+                                        ),
+
+
+                                      ),
+
+
+                                    ),
+                                    Text('$v')
+
+                                  ],
+                                ),
+                              ),
+
                               actions: [
                                 TextButton(
 
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
                                   child: Text('CANCEL'),
                                 ),
                                 TextButton(
 
-                                  onPressed: () {},
-                                  child: Text('ACCEPT'),
+                                  onPressed: () {
+                                    mylist.add( ElevatedButton(
+                                      child: Row(
+                                        children: [
+                                        Text(varname, style: TextStyle(color: Colors.amber)),
+                                        Text(varnumber.toString(), style: TextStyle(color: Colors.amber)),
+                                        ],
+                                      ),
+                                      style: ElevatedButton.styleFrom(
+
+                                        backgroundColor: Colors.black,
+                                      ),
+                                      onPressed: () {},
+                                    ));
+
+                                  },
+                                  child: Text('ADD Tesbhee'),
                                 ),
                               ],
                             ),
@@ -114,6 +188,87 @@ class _tasbheeState extends State<tasbhee> {
         padding: const EdgeInsets.all(13.0),
         child: Center(
           child: Column(
+            children: [
+              Text('$tnmae', style: TextStyle(color: Colors.amber,fontSize: 30),),
+              SizedBox(height: 20,),
+              Text(Counter.toString(), style: TextStyle(color: Colors.amber,fontSize: 30)),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: CircleAvatar(
+                      radius: 130,
+                      backgroundColor: Colors.transparent,
+
+                      child: ClipOval(
+                        child: Image.asset("assets/c.png",
+                        ),
+
+
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Counter++;
+                      });
+
+                      },
+
+
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 100),
+                      shape: const CircleBorder(),
+                      backgroundColor: Colors.amber,
+                    ),
+                  ),
+
+
+
+
+                  SizedBox(width: 30,),
+                  ElevatedButton(
+                    child: CircleAvatar(
+                      radius: 130,
+                      backgroundColor: Colors.transparent,
+
+                      child: ClipOval(
+                        child: Image.asset("assets/reset.png",
+                        ),
+
+
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        Counter=0;
+                      });
+                    },
+
+
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 100),
+                      shape: const CircleBorder(),
+                      backgroundColor: Colors.amber,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30,),
+              Container(
+                width: 500,
+                height: 300,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/kaba.gif"),
+                      fit: BoxFit.cover),
+                ),
+
+              ),
+
+            ],
 
           )
         ),
