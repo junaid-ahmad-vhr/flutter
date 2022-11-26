91fed5b3c3d3c5357b240d314310019969b270cc
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-11-26 12:23:59 +0500

    Update main.dart

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'ui/climatefile.dart';
 void main()=>
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Climiate",
     home: Climate(),
   ));
