536a7b9b82fc9a7ff15978ac645f1ce9f6565f3d
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2022-11-26 08:33:52 +0500

    Video 93 weather app

---
+++ weatherapp/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'ui/climatefile.dart';
+void Main(){
+  runApp(MaterialApp(
+    title: "Climiate",
+    home: Climate(),
+  ));
+}
---
+++ weatherapp/lib/ui/climatefile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Climate extends StatefulWidget {
+  const Climate({Key? key}) : super(key: key);
+
+  @override
+  State<Climate> createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  } 
+}
