c56b70b527afc106bed20478a251b01b6e55bcfe
Jonny bhai <64838868+jonnybhai007@users.noreply.github.com>
2023-01-11 15:10:57 +0500

    work start

---
+++ final_exam/lib/main.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() async {
+
+  runApp(splash());
+}
+class splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 5),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) =>  login())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/aa.gif"),
+            fit: BoxFit.cover),
+      ),
+    );
+
+  }
+}
